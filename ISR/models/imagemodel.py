0421d8efe03e98c1dbb0f6ec4dd4af9c8a645784
SionePalu <32091638+SionePalu@users.noreply.github.com>
2021-10-05 11:07:58 +1100

    Update imagemodel.py

--- ISR/models/imagemodel.py
+++ ISR/models/imagemodel.py
@@ -7,6 +7,17 @@ from ISR.utils.image_processing import (
     stich_together,
 )
 
+from multiprocessing import Process, Manager
+import multiprocessing as mp
+from time import sleep
+
+def predictSinglePatch(listOfLists: list, isrModel: ImageModel):
+    patchIdx = listOfLists[0]
+    print('patchIdx#', str(patchIdx))
+    patchArray = listOfLists[1]
+    predictedPatchArray = isrModel.predict(patchArray)
+    returnList = [patchIdx, predictedPatchArray]
+    return returnList
 
 class ImageModel:
     """ISR models parent class.
@@ -37,19 +48,44 @@ class ImageModel:
                 lr_img, patch_size=by_patch_of_size, padding_size=padding_size
             )
             numPatches = len(patches)
-            print('patches type: ', type(patches))
-            print('p_shape type: ', type(p_shape))
+            print('patches shape: ', list(patches.shape))
+            print('p_shape shape: ', list(p_shape))
             
-            # return patches
-            for i in range(0, len(patches), batch_size):
-                patstr = imgInfo + "Patch-" + str(i+1) + " of " + str(numPatches)
-                print(patstr)
-                batch = self.model.predict(patches[i: i + batch_size])
-                if i == 0:
-                    collect = batch
-                else:
-                    collect = np.append(collect, batch, axis=0)
-                    print('collect shape: ', list(collect.shape))
+            if len(imgInfo) > 0 and imgInfo == 'domap':
+               print('parallel processing')
+               manager = Manager()
+               listOfLists = manager.list()
+               for i in range(0, len(patches), batch_size):
+                   patchIdx = i
+                   patchArray = patches[i: i + batch_size]
+                   listOfLists.append([patchIdx, patchArray])
+               
+               ncpu = mp.cpu_count()
+               print("Number of processors: ", ncpu)
+               print(' ')
+           
+               pool = mp.Pool(ncpu)
+#     predictSinglePatch(listOfLists: list, isrModel: ImageModel)
+               with Pool(processes=ncpu) as pool:
+                  poolObjList = pool.starmap(predictSinglePatch, zip( listOfLists, repeat(self) ))
+                  
+                  for objL in poolObjList:
+                    if len(objL) > 0:
+                      self.results.append(objL)
+            
+                  pool.close()
+                 
+            else:
+               # return patches
+               for i in range(0, len(patches), batch_size):
+                   patstr = imgInfo + "Patch-" + str(i+1) + " of " + str(numPatches)
+                   print(patstr)
+                   batch = self.model.predict(patches[i: i + batch_size])
+                   if i == 0:
+                       collect = batch
+                   else:
+                       collect = np.append(collect, batch, axis=0)
+                       print('collect shape: ', list(collect.shape))
             
             scale = self.scale
             padded_size_scaled = tuple(np.multiply(p_shape[0:2], scale)) + (3,)
